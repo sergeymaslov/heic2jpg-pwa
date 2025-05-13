04f782d0b7b4e52ebaee21b6ee1ca5b0ca404b86
sergeymaslov <53272841+sergeymaslov@users.noreply.github.com>
2025-05-13 20:21:54 +0300

    Update serviceWorker.js fix /

--- serviceWorker.js
+++ serviceWorker.js
@@ -68,7 +68,7 @@ registerRoute(
 setCatchHandler(async ({ event }) => {
     // Return the precached offline page if a document is being requested
     if (event.request.destination === "document") {
-        return matchPrecache("/offline.html");
+        return matchPrecache("offline.html");
     }
 
     return Response.error();
